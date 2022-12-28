f2c0c277dc82a2e7eab341e0f28c6074d61e998d
Yudhitia Rizky <yudhitiarizky2002@gmail.com>
2022-12-28 19:04:18 +0700

    deploy

--- index.js
+++ index.js
@@ -2,24 +2,40 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
-const router = require("./routes/index.js");
+const router = require("./api/index.js.js");
 
 require('dotenv').config();
 
 const app = express();
 const public = __dirname + "/public/";
 const PORT = process.env.PORT_SERVER || 3002;
+const whitelist = [
+    '*'
+  ];
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public/uploads', express.static(path.join(public, "uploads")));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "*");
+app.use((req, res, next) => {
+    const origin = req.get('referer');
+    const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+    if (isWhitelisted) {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+      res.setHeader('Access-Control-Allow-Credentials', true);
+    }
+    // Pass to next layer of middleware
+    if (req.method === 'OPTIONS') res.sendStatus(200);
+    else next();
+  })
+
+const setContext = (req, res, next) => {
+    if (!req.context) req.context = {};
     next();
-})
+};
+app.use(setContext);
 app.use(router);
 
 app.get('/', function(req, res) {
